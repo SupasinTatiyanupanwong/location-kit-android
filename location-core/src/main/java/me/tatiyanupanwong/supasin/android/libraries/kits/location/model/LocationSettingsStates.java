0c4cf219592441b11f1e64ebc512ba9d4ff5bdf9
Gustavo Rivas <gu.rivasl@gmail.com>
2021-02-19 10:31:25 -0300

    Adds missing java docs

--- location-core/src/main/java/me/tatiyanupanwong/supasin/android/libraries/kits/location/model/LocationSettingsStates.java
+++ location-core/src/main/java/me/tatiyanupanwong/supasin/android/libraries/kits/location/model/LocationSettingsStates.java
@@ -9,20 +9,60 @@ import androidx.annotation.NonNull;
  */
 public interface LocationSettingsStates {
 
+    /**
+     * Indicates if the location is present on the device.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isLocationPresent();
 
+    /**
+     * Indicates if the location is enabled and is usable by the app.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isLocationUsable();
 
+    /**
+     * Indicates if the GPS provider is present on the device.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isGpsPresent();
 
+    /**
+     * Indicates if the GPS provider is present on the device.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isGpsUsable();
 
+    /**
+     * Indicates if the network location provider is present on the device.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isNetworkLocationPresent();
 
+    /**
+     * Indicates if the location is enabled and is usable by the app.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isNetworkLocationUsable();
 
+    /**
+     * Indicates if BLE is present on the device.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isBlePresent();
 
+    /**
+     * Indicates if BLE is enabled and is usable by the app.
+     *
+     * @since 1.1.0
+     */
     @NonNull Boolean isBleUsable();
 
 }
